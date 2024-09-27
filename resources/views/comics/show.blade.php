33af0e3dea837852098e614c8660427e3250e076
RobyLedda94 <robyledda01@yahoo.it>
2024-09-27 17:45:42 +0200

    [UPD] recuperati nel file show i dati del db, al click del bottone mostro le immagini e i dettagli del singolo fumetto

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -5,6 +5,9 @@
         <div class="row">
             <div class="col-12">
                 <h1 class="text-center text-white">Dettaglio Fumetto</h1>
+                <div class="py-3 text-center"><img  src="{{$comic->thumb}}" alt=""></div>
+                <h3 class="text-center text-white">{{$comic->series}}</h3>
+                <p class="text-center text-white">{{$comic->description}}</p>
             </div>
         </div>
     </div>
