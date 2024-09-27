41d9e06f74f9b17ec347471b6f2cbd600372e311
RobyLedda94 <robyledda01@yahoo.it>
2024-09-27 16:03:23 +0200

    [UPD] impostata la rotta su homepage, recuperate views dalla repo dc-comics

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,142 @@
+<footer>
+    <div class="bg-blue">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="content-footer-top py-4">
+                        <ul class="list-unstyled d-flex align-items-center justify-content-around m-0">
+                            <li class="d-flex align-items-center">
+                                <img class="links img-fluid" src="/img/buy-comics-digital-comics.png" alt="">
+                                <a class="mx-2 footer nav-link" href="#">DIGITAL COMICS</a>
+                            </li>
+                            <li class="d-flex align-items-center">
+                                <img class="links img-fluid" src="/img/buy-comics-merchandise.png" alt="">
+                                <a class="mx-2 footer nav-link" href="#">DC MERCHANDISE</a>
+                            </li>
+                            <li class="d-flex align-items-center">
+                                <img class="links img-fluid" src="/img/buy-comics-subscriptions.png" alt="">
+                                <a class="mx-2 footer nav-link" href="#">SUBSCRIPTION</a>
+                            </li>
+                            <li class="d-flex align-items-center">
+                                <img class="links img-fluid" src="/img/buy-comics-shop-locator.png" alt="">
+                                <a class="mx-2 footer nav-link" href="#">COMIC SHOP LOCATOR</a>
+                            </li>
+                            <li class="d-flex align-items-center">
+                                <img class="links img-fluid" src="/img/buy-dc-power-visa.svg" alt="">
+                                <a class="mx-2 footer nav-link" href="#">DC POWER VISA</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class='immagine-sfondo'>
+            <div class='container'>
+                <div class='row'>
+                    <div class='col-12'>
+                        <div class='content-main-bottom d-flex justify-content-between'>
+                            <div class='left d-flex py-4'>
+                                <ul class='list-unstyled'>
+                                    <li>
+                                        <p>DC COMICS</p>
+                                    </li>
+                                    <li>
+                                        Characters
+                                    </li>
+                                    <li>
+                                        Comics
+                                    </li>
+                                    <li>
+                                        Movies
+                                    </li>
+                                    <li>
+                                        TV
+                                    </li>
+                                    <li>
+                                        Games
+                                    </li>
+                                    <li>
+                                        Videos
+                                    </li>
+                                    <li>
+                                        News
+                                    </li>
+                                    <li>
+                                        <p class='mt-3'>DC COMICS</p>
+                                    </li>
+                                    <li>
+                                        Shop DC
+                                    </li>
+                                    <li>
+                                        Shop DC Collectibles
+                                    </li>
+                                </ul>
+                                <ul class='list-unstyled mx-4'>
+                                    <li>
+                                        <p>DC</p>
+                                    </li>
+                                    <li>
+                                        Terms Of Use
+                                    </li>
+                                    <li>
+                                        Privacy policy (New)
+                                    </li>
+                                    <li>
+                                        Ad Choices
+                                    </li>
+                                    <li>
+                                        Advertising
+                                    </li>
+                                    <li>
+                                        Jobs
+                                    </li>
+                                    <li>
+                                        Subscription
+                                    </li>
+                                    <li>
+                                        Talent Workshops
+                                    </li>
+                                    <li>
+                                        CPSC Certificates
+                                    </li>
+                                    <li>
+                                        Ratings
+                                    </li>
+                                    <li>
+                                        Shop Help
+                                    </li>
+                                    <li>
+                                        Contact Us
+                                    </li>
+                                </ul>
+                                <ul class='list-unstyled'>
+                                    <li>
+                                        <p>SITES</p>
+                                    </li>
+                                    <li>
+                                        DC
+                                    </li>
+                                    <li>
+                                        MAD Magazine
+                                    </li>
+                                    <li>
+                                        DC Kids
+                                    </li>
+                                    <li>
+                                        DC Universe
+                                    </li>
+                                    <li>
+                                        DC Power Visa
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class='right'>
+                                <img class='logo-grande' src='/img/dc-logo.png'/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,23 @@
+<header>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="content d-flex align-items-center justify-content-around  my-3">
+                        <img class="img-fluid" src="/img/dc-logo.png" alt="DC Comics">
+                        <ul class="list-unstyled d-flex ">
+                            <li class="px-3"><a class="nav-link fw-bold {{Route::currentRouteName() === 'homepage' ? 'active' : ''}}" href="{{route('homepage')}}">CHARACTERS</a></li>
+                            <li class="px-3"><a class="nav-link fw-bold" href="">COMICS</a></li>
+                            <li class="px-3"><a class="nav-link fw-bold {{Route::currentRouteName() === 'films' ? 'active' : ''}}" href="">MOVIES</a></li>
+                            <li class="px-3"><a class="nav-link fw-bold" href="">TV</a></li>
+                            <li class="px-3"><a class="nav-link fw-bold" href="">GAMES</a></li>
+                            <li class="px-3"><a class="nav-link fw-bold" href="">COLLECTIBLES</a></li>
+                            <li class="px-3"><a class="nav-link fw-bold" href="">VIDEOS</a></li>
+                            <li class="px-3"><a class="nav-link fw-bold" href="">FANS</a></li>
+                            <li class="px-3"><a class="nav-link fw-bold" href="">NEWS</a></li>
+                            <li class="px-3"><a class="nav-link fw-bold" href="">SHOP</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
